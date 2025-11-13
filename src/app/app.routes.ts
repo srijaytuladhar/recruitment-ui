9e8ea38d822798304571d569c4282bc6757b6fc4
dipen.limbu <dipen.limbu@fonepay.com>
2025-11-13 10:56:05 +0545

    RP-3: stepper design for recruitment process further

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,14 +3,14 @@ import { Candidate } from './component/candidate/candidate';
 import { Dashboard } from './component/dashboard/dashboard';
 import { ViewCandidate } from './component/candidate/view-candidate/view-candidate';
 import { CreateCandidate } from './component/candidate/create-candidate/create-candidate';
-import { CandidateProceedFurther } from './component/candidate/candidate-proceed-further/candidate-proceed-further';
+import { CandidateProceedFurtherComponent } from './component/candidate/candidate-proceed-further/candidate-proceed-further';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   { path: 'candidates', component: Candidate },
   { path: 'candidates/view/:id', component: ViewCandidate },
   { path: 'candidates/create', component: CreateCandidate },
-  { path: 'candidates/proceed/:id', component: CandidateProceedFurther },
+  { path: 'candidates/proceed/:id', component: CandidateProceedFurtherComponent },
   { path: 'dashboard', component: Dashboard },
   { path: '**', redirectTo: 'dashboard' },
 ];
